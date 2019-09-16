1c83d4a14bb75011bd058adf5ad0ccf8c1f4f98c
harshal-jrvs <harshal.bilimoria-jrvs.ca@gmail.com>
2019-09-16 18:34:04 +0000

    First Commit

---
+++ scala/src/main/scala/ca/jrvs/App.scala
@@ -0,0 +1,10 @@
+package ca.jrvs
+
+object App {
+  def main(args: Array[String]) {
+    println("Hello World!")
+    println("concat arguments = " + foo(args))
+  }
+
+  def foo(x: Array[String]) = x.foldLeft("")((a, b) => a + b)
+}
